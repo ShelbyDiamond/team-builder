0f307c545c286d1361bd27f0ca2690c06bb05439
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-07-24 18:29:37 -0700

    updated to start entire project over

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 import "./App.css";
-import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
-      <Form />
+      <h2>Team Member Sign Up</h2>
     </div>
   );
 }
